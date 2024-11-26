e7711c4903e991935f9a77b9fd4b3628ab1781bd
Jayeun Park <jayeunpakr0704@gmail.com>
2024-11-26 17:44:03 +0900

    Detailed Screen Design

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,43 @@
 import Link from "next/link";
-
+import FormInput from "./formInput";
+import { EnvelopeIcon, KeyIcon } from "@heroicons/react/20/solid";
+import { UserIcon } from "@heroicons/react/20/solid";
 export default function Home() {
   return (
     <div>
       <div className="flex flex-col p-10 w-full">
-        <form className=" flex flex-col items-center justify-center *:font-medium gap-3">
+        <form className=" flex flex-col items-center justify-center *:font-medium gap-5">
           <h1 className="text-4xl mb-4">🔥</h1>
-          <input
-            className="rounded-2xl border-none ring-2 ring-neutral-300 w-1/2"
-            type="text"
-            placeholder="Email"
-            name="name"
-            required
-          ></input>
-          <input
-            className="rounded-2xl border-none ring-2 ring-neutral-300 w-1/2"
-            type="email"
-            placeholder="Username"
-            name="name"
-            required
-          ></input>
-          <input
-            className="rounded-2xl border-none ring-2 ring-neutral-300 w-1/2"
-            type="password"
-            placeholder="Password"
-            name="name"
-            required
-          ></input>
-          <div className="flex flex-col items-center justify-center w-1/2 rounded-2xl bg-gray-400">
-            <Link href="/login" className=" px-3 py-2 ">
+          <label className="relative justify-center  rounded-3xl bg-white w-[60%]">
+            <EnvelopeIcon className=" absolute size-3 top-[15px] left-[14px]  pointer-events-none text-neutral-500" />
+            <FormInput
+              type="text"
+              name="email"
+              placeholder="Email"
+              required={true}
+            />
+          </label>
+          <label className="relative justify-center  rounded-3xl bg-white w-[60%]">
+            <UserIcon className=" absolute size-3 top-[15px] left-[14px]  pointer-events-none text-neutral-500" />
+            <FormInput
+              type="text"
+              placeholder="Username"
+              name="username"
+              required={true}
+            />
+          </label>
+          <label className="relative justify-center  rounded-3xl bg-white w-[60%]">
+            <KeyIcon className=" absolute size-3 top-[15px] left-[14px]  pointer-events-none text-neutral-500" />
+            <FormInput
+              type="password"
+              placeholder="Password"
+              name="password"
+              required={true}
+            />
+          </label>
+
+          <div className="flex flex-col items-center justify-center rounded-3xl bg-neutral-300 w-[60%]">
+            <Link href="/login" className=" px-3 py-2 font-semibold text-xs">
               Log in
             </Link>
           </div>
