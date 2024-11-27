d3f12bea351afc0452470acea249766bbec3595a
Jayeun Park <jayeunpakr0704@gmail.com>
2024-11-28 02:14:13 +0900

    Refactor

--- app/formInput.tsx
+++ app/formInput.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useRef } from "react";
-
 interface IFormInput {
   type: string;
   placeholder: string;
   name: string;
   required: boolean;
-  error: string | null;
+  error?: string[];
 }
 
 export default function FormInput({
@@ -15,23 +13,17 @@ export default function FormInput({
   required,
   error,
 }: IFormInput) {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  // Focus the input if there is an error
-  useEffect(() => {
-    if (error && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [error]);
   return (
     <>
       <input
-        ref={inputRef}
-        className={`focus:outline focus:outline-offset-4 focus:outline-neutral-400 focus:ring-neutral-300 ring-1 ring-neutral-300 border-none
+        className={`focus:outline focus:outline-offset-4 ring-1 ring-neutral-300 border-none
               rounded-3xl w-full
               px-8
               placeholder:text-neutral-400 placeholder:text-xs placeholder:font-semibold
               ${
-                error ? "focus:outline-red-400 ring-1 focus:ring-red-500" : ""
+                error
+                  ? "focus:outline-red-400 ring-1 focus:ring-red-500"
+                  : " focus:outline-neutral-400 focus:ring-neutral-300"
               }`}
         type={type}
         placeholder={placeholder}
