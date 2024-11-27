5bd6863754d0f71da1784c0e55535c4e1ec36a96
Jayeun Park <jayeunpakr0704@gmail.com>
2024-11-28 02:11:22 +0900

    Change checkcircleIcon

--- app/welcomeDiv.tsx
+++ app/welcomeDiv.tsx
@@ -1,17 +1,10 @@
+import { CheckCircleIcon } from "@heroicons/react/20/solid";
+
 export default function WelcomeDiv() {
   return (
-    <div className="relative bg-green-600 rounded-lg text-xs font-extrabold px-7 py-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        className="size-4 absolute left-2"
-      >
-        <path d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-      </svg>
-      Welcome back!
+    <div className="w-full flex items-center gap-4 py-3 pl-7 rounded-2xl bg-green-500">
+      <CheckCircleIcon className="size-5 stroke-2" />
+      <span className="font-semibold">Welcome back!</span>
     </div>
   );
 }
